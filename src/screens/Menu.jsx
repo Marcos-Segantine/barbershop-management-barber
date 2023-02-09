2266ccf094ea67a4f2de9c3a55f4132a37939422
Marcos Segantine <msegantine8@gmail.com>
2023-02-09 00:57:06 -0300

    Creating menu screen

---
+++ src/screens/Menu.jsx
@@ -0,0 +1,19 @@
+import {View, Text, StyleSheet} from 'react-native';
+
+import {Field} from '../components/Field';
+
+import {globalStyles} from '../globalStyles';
+
+export const Menu = () => {
+  return (
+    <View style={[globalStyles.container, {paddingTop: '30%'}]}>
+      <Field text={'Clientes Agendados'} navigateScreen="" />
+
+      <Field text={'Suas informações'} navigateScreen="" />
+
+      <Field text={'Redefinir senha'} navigateScreen="" />
+    </View>
+  );
+};
+
+const style = StyleSheet.create({});

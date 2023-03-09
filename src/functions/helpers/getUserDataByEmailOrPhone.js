463f02a567c0596832684110983ab0f10ef672fc
Marcos Segantine <msegantine8@gmail.com>
2023-03-09 18:26:42 -0300

    Refactoring getUserDataByEmailOrPhone function

--- src/functions/helpers/getUserDataByEmailOrPhone.js
+++ src/functions/helpers/getUserDataByEmailOrPhone.js
@@ -8,34 +8,26 @@ export const getUserDataByEmailOrPhone = async (
   setModalServiceVisible,
 ) => {
   try {
-    const querySnapshot = await firestore()
-      .collection('users')
-      .where('email', '==', email)
-      .get();
-    if (!querySnapshot.empty) {
-      const userData = querySnapshot.docs[0].data();
-      setSchedule({ ...schedule, client: { ...userData } });
-      setModalServiceVisible(true);
-    } else {
-      const querySnapshot = await firestore()
-        .collection('users')
-        .where('phone', '==', phone)
-        .get();
-      if (!querySnapshot.empty) {
-        const userData = querySnapshot.docs[0].data();
-        setSchedule({ ...schedule, client: { ...userData } });
-        setModalServiceVisible(true);
-      } else {
-        console.log('User not found');
 
-        // setSchedule({ null});
-        setModalServiceVisible(true);
-        return null;
-      }
+    let userDocData
+
+    if (email) {
+      const usersRef = firestore().collection('users').where('email', '==', email);
+      userDocData = (await usersRef.get()).docs[0].data()
     }
+    else if (phone) {
+      const usersRef = firestore().collection('users').where('phone', '==', phone);
+      userDocData = (await usersRef.get()).docs[0].data()
+    } else {
+      setModalServiceVisible(false)
+      throw Error("USER NOT FOUND")
+    }
+
+    setSchedule({ ...schedule, client: { ...userDocData } });
+    setModalServiceVisible(true);
+
   } catch (error) {
     console.log('Error getting user data:', error);
-    // setSchedule({ null});
     setModalServiceVisible(false);
     return null;
   }

0d1fdaca5ddaf6b01cacfb8ef5e0af17c217e4f1
Marcos Segantine <msegantine8@gmail.com>
2023-09-30 23:14:25 -0300

    Updating function that gets available times

--- src/services/schedules/getAvailableTimesByProfessional.js
+++ src/services/schedules/getAvailableTimesByProfessional.js
@@ -1,7 +1,6 @@
 import firestore from '@react-native-firebase/firestore';
 
 import { getDay, getMonth, getYear } from '../../utils/dateHelper';
-import { getCurrentHour } from '../../utils/getCurrentHour';
 
 export const getAvailableTimesByProfessional = async (
     professionalUid,
@@ -21,32 +20,94 @@ export const getAvailableTimesByProfessional = async (
 
         const unavailableTimesRef = firestore().collection("unavailable_times").doc(`${month}_${year}`)
         const workingHoursRef = firestore().collection('working_hours').doc(professionalUid)
+        const blockedTimesRef = firestore().collection('blocked_times').doc(professionalUid)
 
         const workingHoursData = (await workingHoursRef.get({ source: "server" })).data()[weekday]
         const unavailableTimesData = (await unavailableTimesRef.get({ source: "server" })).data()
+        const blockedTimesData = (await blockedTimesRef.get({ source: "server" })).data()
 
-        const currentMonth = new Date().getMonth() + 1;
-        const currentDay = new Date().getDate();
-        const currentDate = Number(currentDay) === Number(day) && Number(currentMonth) === Number(month)
+        const date = new Date()
+        const currentMonth = +date.getMonth() + 1;
+        const currentDay = +date.getDate();
+        const currentHour = +date.getHours()
+        const currentDate = +currentDay === +day && +currentMonth === +month
 
-        const currentHour = getCurrentHour()
+        const daysWithTimesBlocked = Object.keys(blockedTimesData)
+        const currentDayHasBlockedTime = daysWithTimesBlocked.includes(scheduleInfo.day)
+
+        const unavailableTimes = (time) => {
+            // Returns `true` if the time is available
+
+            return !unavailableTimesData[day][professionalUid].includes(time)
+        }
+
+        const blockedTimes = (time) => {
+            // Returns `true` if the time is available
+            // Returns `false` if the time is blocked
+
+            return !blockedTimesData[scheduleInfo.day].includes(time)
+        }
+
+        const unavailableTimesCurrentDays = (time) => {
+            // Returns `true` if the time is available
+
+            time = +time.split(":")[0]
+            return time >= currentHour
+        }
 
         // if professional or day selected don't have any schedule, return all times because he is free
         if (!unavailableTimesData) return workingHoursData
-        else if(unavailableTimesData[day] === undefined) return workingHoursData
-        else if (!unavailableTimesData[day] && currentDate || unavailableTimesData[day] && currentDate) {
-            return workingHoursData.filter(schedule => Number(schedule.split(":")[0]) > Number(currentHour) && !unavailableTimesData[day][professionalUid].includes(schedule))
-        }
-        else if (!unavailableTimesData[day]?.[professionalUid] && currentDate) return workingHoursData.filter(schedule => Number(schedule.split(":")[0]) > Number(currentHour))
 
-        else if (!unavailableTimesData[day]) return workingHoursData
-        else if (!unavailableTimesData[day][professionalUid]) return workingHoursData
+        else if (unavailableTimesData[day] === undefined && currentDate && currentDayHasBlockedTime)
+            return workingHoursData.filter(time => {
+                return unavailableTimesCurrentDays(time) && blockedTimes(time)
+            })
 
-        const dataTemp = workingHoursData.filter(schedule => {
-            return !unavailableTimesData[day][professionalUid].includes(schedule)
-        })
+        else if (unavailableTimesData[day] === undefined && currentDate)
+            return workingHoursData.filter(time => {
+                return unavailableTimesCurrentDays(time)
+            })
 
-        return dataTemp;
+        else if (unavailableTimesData[day] === undefined)
+            return workingHoursData
+
+        else if (unavailableTimesData[day][professionalUid] === undefined && currentDate && currentDayHasBlockedTime)
+            return workingHoursData.filter(time => {
+                return unavailableTimesCurrentDays(time) && blockedTimes(time)
+            })
+
+        else if (unavailableTimesData[day][professionalUid] === undefined && currentDate)
+            return workingHoursData.filter(time => {
+                return unavailableTimesCurrentDays(time)
+            })
+
+        else if (unavailableTimesData[day][professionalUid] === undefined && currentDayHasBlockedTime)
+            return workingHoursData.filter(time => {
+                return blockedTimes(time)
+            })
+
+        else if (unavailableTimesData[day][professionalUid] === undefined)
+            return workingHoursData
+
+        else if (unavailableTimesData[day][professionalUid] && currentDate && currentDayHasBlockedTime)
+            return workingHoursData.filter(time => {
+                return unavailableTimes(time) && unavailableTimesCurrentDays(time) && blockedTimes(time)
+            })
+
+        else if (unavailableTimesData[day][professionalUid] && currentDate)
+            return workingHoursData.filter(time => {
+                return unavailableTimes(time) && unavailableTimesCurrentDays(time)
+            })
+
+        else if (unavailableTimesData[day][professionalUid] && currentDayHasBlockedTime)
+            return workingHoursData.filter(time => {
+                return unavailableTimes(time) && blockedTimes(time)
+            })
+
+        else if (unavailableTimesData[day][professionalUid])
+            return workingHoursData.filter(time => {
+                return unavailableTimes(time)
+            })
 
     } catch (error) {
         console.error(error);
